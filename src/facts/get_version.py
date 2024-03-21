5d35022e70c714e60b7c47c32f4f89cc3f9516fb
Stageddat <chmarc72@gmail.com>
2024-03-21 18:11:44 +0100

    Group get_version into function

---
+++ src/facts/get_version.py
@@ -0,0 +1,34 @@
+import requests
+from config import github_token, raw_fact_list_github, facts_md_path
+
+async def get_version():
+    # Get the github version
+    headers = {'Authorization': f'token {github_token}'}
+    response = requests.get(raw_fact_list_github, headers=headers)
+    version_prefix = "**Version: "
+    version_suffix = "**"
+    content = response.text
+    lines = content.split('\n')
+
+    # get version
+    version_line = next((line for line in lines if line.startswith(version_prefix)), None)
+    if version_line is None:
+        return "Failed to connect Github"
+
+    github_version = int(version_line.strip().replace(version_prefix, "").replace(version_suffix, ""))
+
+    # Get local version
+    with open(facts_md_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    version_prefix = "**Version: "
+    version_suffix = "**"
+
+    # get version
+    version_line = next((line for line in lines if line.startswith(version_prefix)), None)
+    if version_line is None:
+        return "Failed to load local version"
+
+    local_version = int(version_line.strip().replace(version_prefix, "").replace(version_suffix, ""))
+
+    return github_version, local_version
