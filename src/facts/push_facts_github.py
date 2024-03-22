d6ab233403f8bb70795fb41d42bc4bdf44f1ee91
Stageddat <chmarc72@gmail.com>
2024-03-22 13:25:14 +0100

    Add token acces

--- src/facts/push_facts_github.py
+++ src/facts/push_facts_github.py
@@ -1,14 +1,19 @@
+from getpass import getpass
 from git import Repo
+import dotenv
+import os
 
-def push_facts_github(repo_path, file_paths, commit_message, remote_name, remote_url):
+def push_facts_github(repo_path, file_paths, commit_message, remote_name, remote_url, token):
     repo = Repo(repo_path)
 
     repo.index.add(file_paths)
 
     repo.index.commit(commit_message)
 
+    remote_url_with_token = remote_url.replace("https://", f"https://{token}@")
+    
     try:
         origin = repo.remote(name=remote_name)
     except ValueError:
-        origin = repo.create_remote(remote_name, url=remote_url)
+        origin = repo.create_remote(remote_name, url=remote_url_with_token)
     origin.push()
