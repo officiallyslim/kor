930f98781a6d7ca8ae148348253109ae6c4bb32a
Stageddat <chmarc72@gmail.com>
2024-03-25 18:18:33 +0100

    Add faily_count link

--- config.py
+++ config.py
@@ -74,5 +74,6 @@ github_token = str(os.getenv("GITHUB_TOKEN"))
 raw_fact_list_github = f"https://raw.githubusercontent.com/Stageddat/kor/main/src/facts/island_fact/facts_list.md"
 raw_fact_database_github = f"https://raw.githubusercontent.com/Stageddat/kor/main/src/facts/island_fact/island_fact.json"
 raw_added_fact_github = f"https://raw.githubusercontent.com/Stageddat/kor/main/src/factsisland_fact//added_trivia.json"
+raw_daily_count_github = f"https://raw.githubusercontent.com/Stageddat/kor/main/src/facts/island_fact/daily_count.txt"
 
 # Img links
