428b0f70f1d6863c9a1c61da46c02893aba52bea
Stageddat <chmarc72@gmail.com>
2024-03-27 22:00:30 +0100

    Add db path

--- src/global_src/global_path.py
+++ src/global_src/global_path.py
@@ -1,3 +1,4 @@
+# Facts file
 facts_md_path = 'src/facts/island_fact/facts_list.md'
 daily_count_path = 'src/facts/island_fact/daily_count.txt'
 added_trivia_path = 'src/facts/island_fact/added_trivia.json'
@@ -5,14 +6,21 @@ island_fact_database_path = 'src/facts/island_fact/island_fact.json'
 new_fact_path = 'src/facts/new_fact.txt'
 error_fact_path = 'src/facts/error_fact/error_fact.json'
 
+# Embed sender
 embed_path = 'src/embed_sender/embed.json'
 
+# Welcome image
 default_pfp_path = 'src/new_member/new_member_img/defaultpfp.png'
 pfp_img_path = 'src/new_member/new_member_img/user_pfp.png'
 background_img_path = 'src/new_member/new_member_img/background.png'
 welcome_img_folder_path = 'src/new_member/old_welcome_img/'
 
+# Ticket
+ticket_success_embed_path = "src/ticket/embed/success_ticket.json"
+
+# Pixel art
 pixel_art_panel_embed_path = "src/ticket/embed/pixel_art_panel.json"
 pixel_art_welcome_embed_path = "src/ticket/embed/pixel_art_welcome.json"
 
-ticket_success_embed_path = "src/ticket/embed/success_ticket.json"
+# Database
+ticket_database_path = "src/ticket/db/ticket.db"
