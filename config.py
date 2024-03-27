50181380125708811864d08c728fc422f2d10019
Stageddat <chmarc72@gmail.com>
2024-03-27 12:12:48 +0100

    Move raw links

--- config.py
+++ config.py
@@ -47,9 +47,4 @@ fact_list_github = "https://github.com/Stageddat/kor/blob/main/src/facts/island_
 dotenv.load_dotenv()
 github_token = str(os.getenv("GITHUB_TOKEN"))
 
-raw_fact_list_github = f"https://raw.githubusercontent.com/Stageddat/kor/main/src/facts/island_fact/facts_list.md"
-raw_fact_database_github = f"https://raw.githubusercontent.com/Stageddat/kor/main/src/facts/island_fact/island_fact.json"
-raw_added_fact_github = f"https://raw.githubusercontent.com/Stageddat/kor/main/src/facts/island_fact/added_trivia.json"
-raw_daily_count_github = f"https://raw.githubusercontent.com/Stageddat/kor/main/src/facts/island_fact/daily_count.txt"
-
 # Img links
---
+++ src/global_src/global_raw_links.py
@@ -0,0 +1,4 @@
+raw_fact_list_github = f"https://raw.githubusercontent.com/Stageddat/kor/main/src/facts/island_fact/facts_list.md"
+raw_fact_database_github = f"https://raw.githubusercontent.com/Stageddat/kor/main/src/facts/island_fact/island_fact.json"
+raw_added_fact_github = f"https://raw.githubusercontent.com/Stageddat/kor/main/src/facts/island_fact/added_trivia.json"
+raw_daily_count_github = f"https://raw.githubusercontent.com/Stageddat/kor/main/src/facts/island_fact/daily_count.txt"
