b93df0cc2755f2ef5a22286845a7a157e75f5533
Stageddat <chmarc72@gmail.com>
2024-03-27 23:04:57 +0100

    Fix placeholder

--- src/global_src/global_embed.py
+++ src/global_src/global_embed.py
@@ -2,24 +2,24 @@ from config import discord
 
 no_perm_embed = discord.Embed(
     title="",
-    description=f"You are not allowed to use this command!",
+    description="You are not allowed to use this command!",
     colour=discord.Colour(int("ff0000", 16))
 )
 
 soon_embed = discord.Embed(
     title="Soon!",
-    description=f"This command is not available for now. Try again later!",
+    description="This command is not available for now. Try again later!",
     colour=discord.Colour(int("ff0000", 16))
 )
 
 error_embed = discord.Embed(
     title="Something failed :c",
-    description=f"Please, contact <@756509638169460837> or any moderator/administrator.",
+    description="Please, contact <@756509638169460837> or any moderator/administrator.",
     colour=discord.Colour(int("ff0000", 16))
 )
 
 failed_fetch_daily_channel = discord.Embed(
     title="Daily fact channel log not found!",
-    description=f"Failed log mod action becouse daily fact log channel not found",
+    description="Failed log mod action becouse daily fact log channel not found",
     colour=discord.Colour(int("ff0000", 16))
 )
