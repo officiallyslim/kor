9e9725721a5fe8d81e83c3f236ec0fb67f665a0c
Stageddat <chmarc72@gmail.com>
2024-03-27 17:58:40 +0100

    Add welcome message

--- src/ticket/view/pixel_art.py
+++ src/ticket/view/pixel_art.py
@@ -2,6 +2,8 @@ import discord
 from src.global_src.global_emojis import smile_pixel_emoji
 from src.ticket.utils.create_overwrites import create_overwrites
 from typing import Union
+from src.ticket.utils.gen_ticket_key import gen_key
+from datetime import datetime
 
 class pixel_art_panel_view(discord.ui.View):
     def __init__(self):
@@ -9,21 +11,22 @@ class pixel_art_panel_view(discord.ui.View):
 
     @discord.ui.button(label="Pixel Art", style=discord.ButtonStyle.primary, emoji=smile_pixel_emoji, custom_id="pixel_art_panel_button")
     async def pixel_art_panel_button(self, button: discord.ui.Button, interaction: discord.Interaction):
-        print("Creating new ticket")
-        member_who_interacted = interaction.user
+        ticket_id = gen_key(15)
+        print(f"Creating new ticket: {ticket_id}")
+
+        whoami = interaction.user
         mod_role = interaction.guild.get_role(1222579667207192626)
 
-        # Crea una tupla con los objetos que quieres incluir en los overwrites.
-        objects = (member_who_interacted, mod_role)
+        objects = (whoami, mod_role)
 
-        # Llama a la función create_overwrites con los objetos desempaquetados.
         overwrites = create_overwrites(interaction, *objects)
 
-        # Ahora puedes crear el canal de texto con los overwrites.
-        await interaction.guild.create_text_channel(
-            name="nombre_del_canal",
+        new_channel = await interaction.guild.create_text_channel(
+            name=f"{interaction.user.name} - pixel",
             overwrites=overwrites,
-            topic="Descripción del canal",
-            reason="Razón para crear el canal",
-            category=discord.Object(id=1222316215884582924)  # Asegúrate de que este ID de categoría sea correcto
+            topic=f"{interaction.user.name} Pixel Art Builder request\n Created at <t:{int(datetime.now().timestamp())}>",
+            reason="New channel for Pixel Art Builder request",
+            category=discord.Object(id=1222316215884582924)
         )
+        channel_id = new_channel.id
+        await new_channel.send("Welcome pookie lmao")
