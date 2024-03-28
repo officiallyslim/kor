5067963b13e41099937af56baed7414880b0e50e
Stageddat <chmarc72@gmail.com>
2024-03-28 20:07:02 +0100

    Add action in pixel art tickets

---
+++ src/ticket/view/actions_pixel_art.py
@@ -0,0 +1,17 @@
+import discord
+from src.ticket.modal.form_pixel_art import form_pixel_art_modal
+from src.global_src.global_emojis import claim_emoji
+
+class actions_pixel_art_view(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Claim", style=discord.ButtonStyle.gray, emoji=claim_emoji, custom_id="claim_actions_pixel_art_view")
+    async def claim_form_pixel_art_view(self, button: discord.ui.Button, interaction: discord.Interaction):
+        modal = form_pixel_art_modal(title="Pixel Art Form",name=interaction.user.name)
+        await interaction.response.send_modal(modal)
+
+    @discord.ui.button(label="Close", style=discord.ButtonStyle.red, emoji="🔒", custom_id="close_form_pixel_art_view")
+    async def close_form_pixel_art_view(self, button: discord.ui.Button, interaction: discord.Interaction):
+        modal = form_pixel_art_modal(title="Pixel Art Form",name=interaction.user.name)
+        await interaction.response.send_modal(modal)
