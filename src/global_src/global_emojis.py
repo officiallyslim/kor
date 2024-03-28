5355eb27df27d2b08b7378a2c977bd17f2df6d06
Stageddat <chmarc72@gmail.com>
2024-03-28 19:31:24 +0100

    Change claim emoji

--- src/global_src/global_emojis.py
+++ src/global_src/global_emojis.py
@@ -1,3 +1,3 @@
 smile_pixel_emoji = "<:smile_pixel:1222512011326390272>"
-claim_emoji = "<:claim:1222917682668769480>"
+claim_emoji = "<:claim:1222976228823404645>"
 send_emoji = "<:send:1222919710438592593>"
