5547b87e3ce13e253378986e5ceb4deba4119085
Stageddat <chmarc72@gmail.com>
2024-03-29 00:58:23 +0100

    Fix unique names

--- src/ticket/view/pixel_art_views/actions_pixel_art.py
+++ src/ticket/view/pixel_art_views/actions_pixel_art.py
@@ -7,11 +7,11 @@ class actions_pixel_art_view(discord.ui.View):
         super().__init__(timeout=None)
 
     @discord.ui.button(label="Claim", style=discord.ButtonStyle.gray, emoji=claim_emoji, custom_id="claim_actions_pixel_art_view")
-    async def claim_form_pixel_art_view(self, button: discord.ui.Button, interaction: discord.Interaction):
+    async def claim_actions_pixel_art_view(self, button: discord.ui.Button, interaction: discord.Interaction):
         modal = form_pixel_art_modal(title="Pixel Art Form",name=interaction.user.name)
         await interaction.response.send_modal(modal)
 
-    @discord.ui.button(label="Close", style=discord.ButtonStyle.red, emoji="🔒", custom_id="close_form_pixel_art_view")
-    async def close_form_pixel_art_view(self, button: discord.ui.Button, interaction: discord.Interaction):
+    @discord.ui.button(label="Close", style=discord.ButtonStyle.red, emoji="🔒", custom_id="close_actions_pixel_art_view")
+    async def close_actions_pixel_art_view(self, button: discord.ui.Button, interaction: discord.Interaction):
         modal = form_pixel_art_modal(title="Pixel Art Form",name=interaction.user.name)
         await interaction.response.send_modal(modal)
