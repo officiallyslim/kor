8a5dba4ee004b54d9269e9fdb5b06d63a8abafe8
Stageddat <chmarc72@gmail.com>
2024-03-28 15:01:58 +0100

    Action to pixel art database

--- src/ticket/utils/db_utils/add_db_pixel_art.py
+++ src/ticket/utils/db_utils/add_db_pixel_art.py
@@ -31,7 +31,7 @@ def add_db_pixel_art(
     cursor.execute(
         """
         INSERT INTO pixel_art (ticket_id, open_user_id, open_time, open_reason, form_name, form_roblox_user, form_island_code, form_build, form_build_desp, form_build_img, channel_id, welcome_msg_id, dm_msg_id, queue_msg_id, log_msg_id, transcript_thread_id, claim_status, claim_user_id, close_user_id, close_time, close_reason)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """,
         (
             ticket_id,
