a279d67b46442ab5bf915e955b420749dfacc24a
Stageddat <chmarc72@gmail.com>
2024-03-29 18:59:14 +0100

    Fix custom id and callback in actions of pixel art

--- src/ticket/view/pixel_art_views/actions_pixel_art.py
+++ src/ticket/view/pixel_art_views/actions_pixel_art.py
@@ -5,10 +5,10 @@ class actions_pixel_art_view(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
-    @discord.ui.button(label="Claim", style=discord.ButtonStyle.gray, emoji=claim_emoji, custom_id="claim_actions_pixel_art_view")
-    async def claim_actions_pixel_art_view(self, button: discord.ui.Button, interaction: discord.Interaction):
+    @discord.ui.button(label="Claim", style=discord.ButtonStyle.gray, emoji=claim_emoji, custom_id="claim_actions_pixel_art_button")
+    async def claim_actions_pixel_art_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
         pass
 
-    @discord.ui.button(label="Close", style=discord.ButtonStyle.red, emoji="🔒", custom_id="close_actions_pixel_art_view")
-    async def close_actions_pixel_art_view(self, button: discord.ui.Button, interaction: discord.Interaction):
+    @discord.ui.button(label="Close", style=discord.ButtonStyle.red, emoji="🔒", custom_id="close_actions_pixel_art_button")
+    async def close_actions_pixel_art_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
         pass
