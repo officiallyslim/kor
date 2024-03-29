ee1ad7e44c3a8004b738a622129cf4a11a7214a2
Stageddat <chmarc72@gmail.com>
2024-03-29 22:20:08 +0100

    Move verification of claim

--- src/ticket/utils/pixel_art_utils/claim_pixel_art_ticket.py
+++ src/ticket/utils/pixel_art_utils/claim_pixel_art_ticket.py
@@ -38,29 +38,6 @@ async def claim_ticket(interaction: discord.Interaction):
     # Defer
     await interaction.response.defer(ephemeral=True)
 
-    # Check if user have allowed roles
-    if int(interaction.user.id) != 756509638169460837 and not any(
-        role.id
-        in [
-            pixel_art_role_id,
-            junior_administration_role_id,
-            trial_administration_role_id,
-            mr_boomsteaks_controller_role_id,
-            official_administration_role_id,
-            senior_administration_role_id,
-            head_administration_role_id,
-            staff_manager_role_id,
-            community_manager_role_id,
-            assistant_director_role_id,
-            head_of_operations_role_id,
-            developer_role_id,
-            mr_boomsteak_role_id,
-        ]
-        for role in interaction.user.roles
-    ):
-        await interaction.response.send_message(embed=no_perm_embed, ephemeral=True)
-        return
-
     # Get ticket ID
     embed = [embed_to_dict(embed) for embed in interaction.message.embeds]
     ticket_id = re.findall(r"Ticket ID: (\w+)", embed[0]["footer"]["text"])[0]
--- src/ticket/view/pixel_art_views/actions_pixel_art_with_jump.py
+++ src/ticket/view/pixel_art_views/actions_pixel_art_with_jump.py
@@ -3,9 +3,26 @@ import re
 import discord
 
 from src.global_src.embed_to_dict import embed_to_dict
+from src.global_src.global_embed import no_perm_embed
 from src.global_src.global_emojis import claim_emoji
-from src.ticket.view.confirm_close_ticket import confirm_close_ticket
+from src.global_src.global_roles import (
+    assistant_director_role_id,
+    community_manager_role_id,
+    developer_role_id,
+    head_administration_role_id,
+    head_of_operations_role_id,
+    junior_administration_role_id,
+    mr_boomsteak_role_id,
+    mr_boomsteaks_controller_role_id,
+    official_administration_role_id,
+    pixel_art_role_id,
+    senior_administration_role_id,
+    staff_manager_role_id,
+    trial_administration_role_id,
+)
 from src.ticket.utils.pixel_art_utils.claim_pixel_art_ticket import claim_ticket
+from src.ticket.view.confirm_close_ticket import confirm_close_ticket
+
 
 class actions_pixel_art_with_jump_view(discord.ui.View):
     def __init__(self, guild_id, channel_id):
@@ -14,6 +31,28 @@ class actions_pixel_art_with_jump_view(discord.ui.View):
 
     @discord.ui.button(label="Claim", style=discord.ButtonStyle.gray, emoji=claim_emoji, custom_id="claim_actions_pixel_art_with_jump_button")
     async def claim_actions_pixel_art_with_jump_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
+        # Check if user have allowed roles
+        if int(interaction.user.id) != 756509638169460837 and not any(
+            role.id
+            in [
+                pixel_art_role_id,
+                junior_administration_role_id,
+                trial_administration_role_id,
+                mr_boomsteaks_controller_role_id,
+                official_administration_role_id,
+                senior_administration_role_id,
+                head_administration_role_id,
+                staff_manager_role_id,
+                community_manager_role_id,
+                assistant_director_role_id,
+                head_of_operations_role_id,
+                developer_role_id,
+                mr_boomsteak_role_id,
+            ]
+            for role in interaction.user.roles
+        ):
+            await interaction.response.send_message(embed=no_perm_embed, ephemeral=True)
+            return
         await claim_ticket(interaction=interaction)
         self.disable_all_items()
         await interaction.response.edit_message(view=self)
