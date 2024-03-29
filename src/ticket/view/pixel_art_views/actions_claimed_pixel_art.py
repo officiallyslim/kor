9a307f70bf6d7e5622d005237572179c8d39da2c
Stageddat <chmarc72@gmail.com>
2024-03-29 22:56:32 +0100

    Change verification of user in unclaim button

--- src/ticket/view/pixel_art_views/actions_claimed_pixel_art.py
+++ src/ticket/view/pixel_art_views/actions_claimed_pixel_art.py
@@ -3,26 +3,10 @@ import re
 import discord
 
 from src.global_src.embed_to_dict import embed_to_dict
-from src.global_src.global_embed import no_perm_embed
 from src.global_src.global_emojis import claim_emoji
-from src.global_src.global_roles import (
-    assistant_director_role_id,
-    community_manager_role_id,
-    developer_role_id,
-    head_administration_role_id,
-    head_of_operations_role_id,
-    junior_administration_role_id,
-    mr_boomsteak_role_id,
-    mr_boomsteaks_controller_role_id,
-    official_administration_role_id,
-    pixel_art_role_id,
-    senior_administration_role_id,
-    staff_manager_role_id,
-    trial_administration_role_id,
-)
 from src.ticket.utils.pixel_art_utils.unclaim_pixel_art_ticket import unclaim_ticket
 from src.ticket.view.confirm_close_ticket import confirm_close_ticket
-
+from src.ticket.utils.pixel_art_utils.db_utils.get_db_data_pixel_art import check_claimed_pixeL_art_ticket
 
 class actions_claimed_pixel_art_view(discord.ui.View):
     def __init__(self):
@@ -30,28 +14,14 @@ class actions_claimed_pixel_art_view(discord.ui.View):
 
     @discord.ui.button(label="Unclaim", style=discord.ButtonStyle.gray, emoji=claim_emoji, custom_id="claim_claimed_actions_pixel_art_button")
     async def claim_claimed_actions_pixel_art_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
-        # Check if user have allowed roles
-        if int(interaction.user.id) != 756509638169460837 and not any(
-            role.id
-            in [
-                pixel_art_role_id,
-                junior_administration_role_id,
-                trial_administration_role_id,
-                mr_boomsteaks_controller_role_id,
-                official_administration_role_id,
-                senior_administration_role_id,
-                head_administration_role_id,
-                staff_manager_role_id,
-                community_manager_role_id,
-                assistant_director_role_id,
-                head_of_operations_role_id,
-                developer_role_id,
-                mr_boomsteak_role_id,
-            ]
-            for role in interaction.user.roles
-        ):
-            await interaction.response.send_message(embed=no_perm_embed, ephemeral=True)
-            return
+        # Get ticket_id
+        embed = [embed_to_dict(embed) for embed in interaction.message.embeds]
+        ticket_id = re.findall(r"Ticket ID: (\w+)", embed[0]["footer"]["text"])[0]
+
+        # Check if user is the claimer
+        claimed_users_id = check_claimed_pixeL_art_ticket(ticket_id)
+        if interaction.user.id not in claimed_users_id:
+            await interaction.response.send_message("Sorry, only claim user can unclaim.", ephemeral=True)
         button.disabled = True
         await interaction.response.edit_message(view=self)
         await unclaim_ticket(interaction=interaction)
