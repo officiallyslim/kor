05f674c38c4cd8d1548ef838c66526ea7eded7af
Stageddat <chmarc72@gmail.com>
2024-03-29 15:35:29 +0100

    Add log message

--- src/ticket/utils/transcript_website.py
+++ src/ticket/utils/transcript_website.py
@@ -2,6 +2,7 @@ import discord
 import chat_exporter
 
 async def get_transcript(channel: discord.TextChannel):
+    print("Creating transcript...")
     export = await chat_exporter.export(channel=channel)
     file_name=f"{channel.id}.html"
     with open(file_name, "w", encoding="utf-8") as f:
