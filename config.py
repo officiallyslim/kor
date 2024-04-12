047234283f4e096b52d16666b7608e59ce8d1c5e
Stageddat <chmarc72@gmail.com>
2024-04-12 22:10:05 +0200

    Remove unused links

--- config.py
+++ config.py
@@ -13,10 +13,6 @@ bot = commands.Bot(command_prefix="a!", intents=intents)
 
 admins = [756509638169460837]
 
-# Links
-island_base_url = "https://robloxislands.fandom.com"
-fact_list_github = "https://github.com/Stageddat/kor/blob/main/src/facts/island_fact/facts_list.md"
-
 # Server ID
 guild_id = 1151612424785494116
 
