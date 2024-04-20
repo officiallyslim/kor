6ce41306145815a3aba9803d0fe052e8c4ab34de
Stageddat <chmarc72@gmail.com>
2024-04-21 00:57:21 +0200

    Add Fill button verification

--- src/ticket/view/builder_request_views/form_builder_request.py
+++ src/ticket/view/builder_request_views/form_builder_request.py
@@ -3,10 +3,14 @@ import re
 import discord
 
 from src.global_src.embed_to_dict import embed_to_dict
+from src.global_src.global_embed import error_embed, no_perm_embed
 from src.ticket.modal.form_builder_request import builder_request_modal
+from src.ticket.utils.builder_request_utils.db_utils.get_db_data_builder_request import (
+    get_builder_open_user_id,
+    get_builder_ticket_type,
+)
 from src.ticket.view.confirm_close_ticket import confirm_close_ticket
-from src.global_src.global_embed import error_embed
-from src.ticket.utils.builder_request_utils.db_utils.get_db_data_builder_request import get_builder_ticket_type
+
 
 class form_builder_request_view(discord.ui.View):
     def __init__(self):
@@ -22,6 +26,12 @@ class form_builder_request_view(discord.ui.View):
                 ticket_id = match[0]
         except Exception:
             await interaction.response.send_message(embed=error_embed, ephemeral=True)
+            return
+
+        # Verify user
+        open_user_id = get_builder_open_user_id(ticket_id)
+        if open_user_id is not None and int(interaction.user.id) != int(open_user_id):
+            await interaction.response.send_message(embed=no_perm_embed, ephemeral=True)
             return
 
         ticket_type = get_builder_ticket_type(ticket_id=ticket_id)
