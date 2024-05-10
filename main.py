b3113b0fe9a9982b1a3b941f26f740f46b9a03e2
Stageddat <chmarc72@gmail.com>
2024-05-10 16:39:19 +0200

    test: change cogs loading

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from src.global_src.global_roles import (
 dotenv.load_dotenv()
 token = str(os.getenv("TOKEN"))
 
-cogs = ["cogs.ticket"]
+cogs = ["cogs.loa_request"]
 
 for cog in cogs:
     try:
