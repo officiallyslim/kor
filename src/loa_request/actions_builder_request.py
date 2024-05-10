6d8e5964aebbf52149f9bde4b8e4ab9538d104df
Stageddat <chmarc72@gmail.com>
2024-05-10 16:39:07 +0200

    test: request loa view

---
+++ src/loa_request/actions_builder_request.py
@@ -0,0 +1,17 @@
+import discord
+
+
+class actions_builder_view(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(
+        label="Request",
+        style=discord.ButtonStyle.green,
+        emoji="😩",
+        custom_id="request_loa_callback",
+    )
+    async def request_loa_callback(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        pass
