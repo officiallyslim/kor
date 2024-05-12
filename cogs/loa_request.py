aa9c18e740c7d2799e792af5201b0e8e00b94322
Stageddat <chmarc72@gmail.com>
2024-05-12 16:45:39 +0200

    feat: get max builder role

--- cogs/loa_request.py
+++ cogs/loa_request.py
@@ -2,28 +2,46 @@ import discord
 from discord.ext import commands
 from src.loa_request.data.roles_hierarchy import builder_roles_hierarchy
 
+
+def get_max_builder(auhor_roles):
+    levels = [
+        builder_roles_hierarchy.get(role.id, {}).get("level", 0) for role in auhor_roles
+    ]
+
+    max_level = max(levels, default=None)
+
+    if max_level is not None:
+        max_role = next(
+            (
+                role
+                for role in auhor_roles
+                if builder_roles_hierarchy.get(role.id, {}).get("level") == max_level
+            ),
+            None,
+        )
+        if max_role:
+            return max_role
+        else:
+            return "Error"
+    else:
+        return "No builder"
+
+
 class loa_request(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    @discord.slash_command(name="max_builder_test", description="Test the welcome feature with specific member")
+
+    @discord.slash_command(
+        name="max_builder_test",
+        description="Test the welcome feature with specific member",
+    )
     async def test_welcome(self, ctx: discord.ApplicationContext):
         await ctx.defer(ephemeral=True)
 
         author_roles = ctx.author.roles
 
-        levels = [builder_roles_hierarchy.get(role.id, {}).get("level", 0) for role in author_roles]
-
-        max_level = max(levels, default=None)
-
-        if max_level is not None:
-            rol_mas_alto = next((role for role in author_roles if builder_roles_hierarchy.get(role.id, {}).get("level") == max_level), None)
-            if rol_mas_alto:
-                await ctx.respond(f"Max rol: {rol_mas_alto.mention} (level {max_level}).", ephemeral=True)
-            else:
-                await ctx.respond("Bigger role not found", ephemeral=True)
-        else:
-            await ctx.respond("No builder roles found", ephemeral=True)
+        max_role = get_max_builder(author_roles)
+        await ctx.respond(max_role, ephemeral=True)
 
 
 def setup(bot):
