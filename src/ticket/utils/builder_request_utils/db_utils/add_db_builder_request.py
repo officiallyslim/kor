1348acfdef88198f3e08c3cb4cec2716824d2d08
Stageddat <chmarc72@gmail.com>
2024-05-26 00:46:50 +0200

    feat: add new form question in add builder

--- src/ticket/utils/builder_request_utils/db_utils/add_db_builder_request.py
+++ src/ticket/utils/builder_request_utils/db_utils/add_db_builder_request.py
@@ -14,6 +14,7 @@ def add_builder_request_db(
     form_build,
     form_build_desp,
     form_build_img,
+    form_payment,
     channel_id,
     welcome_msg_id,
     dm_msg_id,
@@ -31,8 +32,8 @@ def add_builder_request_db(
 
     cursor.execute(
         """
-        INSERT INTO builder_request (ticket_id,ticket_type, open_user_id, open_time, open_reason, form_name, form_roblox_user, form_island_code, form_build, form_build_desp, form_build_img, channel_id, welcome_msg_id, dm_msg_id, confirm_message_id, queue_msg_id, log_msg_id, transcript_key, claim_user_id, close_user_id, close_time, close_reason)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO builder_request (ticket_id,ticket_type, open_user_id, open_time, open_reason, form_name, form_roblox_user, form_island_code, form_build, form_build_desp, form_build_img, form_payment, channel_id, welcome_msg_id, dm_msg_id, confirm_message_id, queue_msg_id, log_msg_id, transcript_key, claim_user_id, close_user_id, close_time, close_reason)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """,
         (
             ticket_id,
@@ -46,6 +47,7 @@ def add_builder_request_db(
             form_build,
             form_build_desp,
             form_build_img,
+            form_payment,
             channel_id,
             welcome_msg_id,
             dm_msg_id,
