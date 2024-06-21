efae9407b89409dbd0062270ee4139abb00b504e
Stageddat <chmarc72@gmail.com>
2024-06-21 21:02:05 +0200

    feat: add embed to claimed ticket type

--- src/ticket/utils/builder_request_utils/db_utils/ticket_info_embed.py
+++ src/ticket/utils/builder_request_utils/db_utils/ticket_info_embed.py
@@ -35,3 +35,35 @@ def create_ticket_info_embed(ticket_data: Ticket, current_ticket_status):
         )
         info_embed.set_footer(text=f"Ticket ID: {ticket_data.ticket_id}")
         return info_embed
+    elif current_ticket_status == f"Currently claimed by <@{ticket_data.claim_user_id}>":
+        info_embed = discord.Embed(
+            title=f"Ticket {ticket_data.ticket_id} information",
+            description=f"""
+                        **📢 Current status: {current_ticket_status}** 
+                        **🙍 Open user:** <@{ticket_data.open_user_id}>
+                        **💁 Claim user:** <@{ticket_data.claim_user_id}>
+                        **❓ Ticket type:** <@{ticket_data.open_user_id}>
+                        **🕑 Open Time:** <t:{ticket_data.open_time}:F>
+                        **🏠 Channel:** <#{ticket_data.channel_id}>""",
+            colour=discord.Colour(int("5cb85c", 16)),
+        )
+        info_embed.add_field(
+            name="Roblox user (form)",
+            value=f"```{ticket_data.form_roblox_user}```",
+            inline=False,
+        )
+        info_embed.add_field(
+            name="Island Code (form)",
+            value=f"```{ticket_data.form_island_code}```",
+            inline=False,
+        )
+        info_embed.add_field(
+            name="Build (form)", value=f"```{ticket_data.form_build}```", inline=False
+        )
+        info_embed.add_field(
+            name="Payment (form)",
+            value=f"```{ticket_data.form_payment}```",
+            inline=False,
+        )
+        info_embed.set_footer(text=f"Ticket ID: {ticket_data.ticket_id}")
+        return info_embed
