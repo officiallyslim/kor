9b07d30272fbe79f8a3f6095b95ec8bb8c91a8a9
Stageddat <chmarc72@gmail.com>
2024-10-01 19:09:23 +0200

    refactor: tickets commands

--- commands/ticket_commands.py
+++ commands/ticket_commands.py
@@ -1,4 +1,5 @@
 import discord
+from commands.ticket_cmd_group.ticket_claim import claim_ticket_cmd
 from commands.ticket_cmd_group.ticket_remove import remove_user_ticket
 from commands.ticket_cmd_group.ticket_close import close_ticket
 from commands.ticket_cmd_group.ticket_add import add_user_ticket
@@ -14,24 +15,19 @@ ticket_group.command(
 
 ticket_group.command(name="close", description="Close the ticket")(
     option("close_reason", description="Close ticket reason")(
-        option(
-            "ticket_id",
-            description="The ID of the ticket you want to close",
-            default=None,
-        )(
-            option(
-                "password",
-                description="Close the ticket even if it's claimed",
-                default=None,
-            )(close_ticket)
-        )
-    )
-)
+    option("ticket_id", description="The ID of the ticket you want to close", default=None,)(
+    option("password", description="Close the ticket even if it's claimed", default=None,)
+    (close_ticket))))
 
 ticket_group.command(
     name="add", description="Add someone to the current ticket channel"
 )(add_user_ticket)
 
-ticket_group.command(name="info", description="View specific information")(
-    option("ticket_id", description="The ticket ID for view information", default=None)(view_ticket_info_callback)
-)
+ticket_group.command(
+    name="info", description="View specific information")(
+    option("ticket_id", description="The ticket ID for view information", default=None)
+    (view_ticket_info_callback))
+
+ticket_group.command(
+    name="claim", description="Claim the current ticket channel"
+)(claim_ticket_cmd)
