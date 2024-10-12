0ca180e9b833aced2865f9583388b7129dd7da3e
Stageddat <chmarc72@gmail.com>
2024-10-12 13:05:37 +0200

    fix: break claim system after error

--- src/ticket/utils/builder_request_utils/claim_builder_request_ticket.py
+++ src/ticket/utils/builder_request_utils/claim_builder_request_ticket.py
@@ -20,15 +20,18 @@ from src.ticket.utils.create_overwrites import create_custom_overwrites
 from src.utils.embed_to_dict import embed_to_dict
 
 
-async def claim_ticket(interaction: discord.Interaction, ticket_id = None):
+async def claim_ticket(interaction: discord.Interaction, ticket_id=None):
     print(ticket_id)
     if ticket_id is None:
         embed = [embed_to_dict(embed) for embed in interaction.message.embeds]
-        ticket_id = {"origin": "button", "ticket_id":re.findall(r"Ticket ID: (\w+)", embed[0]["footer"]["text"])[0]}
+        ticket_id = {
+            "origin": "button",
+            "ticket_id": re.findall(r"Ticket ID: (\w+)", embed[0]["footer"]["text"])[0],
+        }
 
-    # Get ticket type
     ticket_type = get_builder_ticket_type(ticket_id=str(ticket_id["ticket_id"]))
     found_ticket = False
+
     for key, value in ticket_type_dict.items():
         if str(value["type"]) == str(ticket_type):
             queue_channel_id = value["queue_channel_id"]
@@ -36,12 +39,22 @@ async def claim_ticket(interaction: discord.Interaction, ticket_id = None):
             builder_role_id = value["role_id"]
             found_ticket = True
             break
-        if not found_ticket:
-            print("No ticket type found!")
-            print(f"Ticket type provided: {ticket_type}")  # Imprimir el tipo de ticket proporcionado
+
+    if not found_ticket:
+        print("No ticket type found!")
+        print(f"Ticket type provided: {ticket_type}")
+        if ticket_id["origin"] == "button":
+            await interaction.followup.send(
+                "Something failed. Please, report to stage", ephemeral=True
+            )
             return
-        else:
-            print(f"Builder role ID: {builder_role_id}")  # Solo imprimir si se encontró el ticket
+        elif ticket_id["origin"] == "cmd":
+            await interaction.response.send_message(
+                "Something failed. Please, report to stage", ephemeral=True
+            )
+            return
+    else:
+        print(f"Builder role ID: {builder_role_id}")
 
     # Get channel ID
     channel_id = get_builder_channel_id(ticket_id=ticket_id["ticket_id"])
@@ -57,11 +70,9 @@ async def claim_ticket(interaction: discord.Interaction, ticket_id = None):
     # Set roles perms
     new_overwrites = create_custom_overwrites(
         interaction,
-        no_perm_objects=(
-            builder_role,
-        ),
+        no_perm_objects=(builder_role,),
         view_only_objects=(),
-        view_and_chat_objects=(whoami,open_user ),
+        view_and_chat_objects=(whoami, open_user),
         moderator_objects=(),
     )
 
@@ -72,12 +83,15 @@ async def claim_ticket(interaction: discord.Interaction, ticket_id = None):
             await ticket_channel.set_permissions(obj, overwrite=perms)
     except Exception as e:
         if ticket_id["origin"] == "button":
-            await interaction.followup.send(f"Something failed. Please, report to stage: {e}", ephemeral=True)
+            await interaction.followup.send(
+                f"Something failed. Please, report to stage: {e}", ephemeral=True
+            )
             return
         elif ticket_id["origin"] == "cmd":
-            await interaction.response.send_message(f"Something failed. Please, report to stage: {e}", ephemeral=True)
+            await interaction.response.send_message(
+                f"Something failed. Please, report to stage: {e}", ephemeral=True
+            )
             return
-
 
     # Respond and edit
     if ticket_id["origin"] == "button":
@@ -87,17 +101,20 @@ async def claim_ticket(interaction: discord.Interaction, ticket_id = None):
 
     # Notify
     notification_embed = discord.Embed(
-            title="",
-            description=f"{interaction.user.mention} claimed this ticket!",
-            colour=discord.Colour(int("ff7900", 16)),
-        )
+        title="",
+        description=f"{interaction.user.mention} claimed this ticket!",
+        colour=discord.Colour(int("ff7900", 16)),
+    )
     await interaction.channel.send(embed=notification_embed)
 
-    welcome_msg_id, channel_id = get_builder_welcome_msg(ticket_id=ticket_id["ticket_id"])
+    welcome_msg_id, channel_id = get_builder_welcome_msg(
+        ticket_id=ticket_id["ticket_id"]
+    )
     welcome_msg = await bot.get_channel(channel_id).fetch_message(welcome_msg_id)
     from src.ticket.view.builder_request_views.actions_claimed_builder_request import (
         actions_claimed_builder_view,
     )
+
     try:
         await welcome_msg.edit(view=actions_claimed_builder_view())
     except Exception as e:
@@ -107,32 +124,68 @@ async def claim_ticket(interaction: discord.Interaction, ticket_id = None):
             colour=discord.Colour(int("ff0000", 16)),
         )
         await interaction.followup.send(embeds=[error_code_embed], ephemeral=True)
-        print(f"Error when fetching queue message id for ticket {ticket_id['ticket_id']}: {e}")
+        print(
+            f"Error when fetching queue message id for ticket {ticket_id['ticket_id']}: {e}"
+        )
         return
 
     # Edit queue message
     try:
         queue_message_id = get_builder_queue_message_id(ticket_id["ticket_id"])
         if queue_message_id is not None:
-            queue_message = await bot.get_channel(queue_channel_id).fetch_message(queue_message_id)
+            queue_message = await bot.get_channel(queue_channel_id).fetch_message(
+                queue_message_id
+            )
             old_embed = [embed_to_dict(embed) for embed in queue_message.embeds]
 
             new_embed = discord.Embed(
                 title=f"{builder_name} ticket - {ticket_id['ticket_id']}",
-                color=0x28a745,
-                description=""
+                color=0x28A745,
+                description="",
             )
-            new_embed.add_field(name="👤 User", value=old_embed[0]['fields'][0]['value'], inline=False)
-            new_embed.add_field(name="🆔 User ID", value=old_embed[0]['fields'][1]['value'], inline=False)
-            new_embed.add_field(name="📛 User name", value=old_embed[0]['fields'][2]['value'], inline=False)
-            new_embed.add_field(name="👥 Claim user", value=interaction.user.mention, inline=False)
-            new_embed.add_field(name=f"{discord_emoji} Discord name", value=old_embed[0]['fields'][4]['value'], inline=False)
-            new_embed.add_field(name=f"{roblox_emoji} Roblox username", value=old_embed[0]['fields'][5]['value'], inline=False)
-            new_embed.add_field(name="🔢 Island Code", value=old_embed[0]['fields'][6]['value'], inline=False)
-            new_embed.add_field(name="🏠 Build", value=old_embed[0]['fields'][7]['value'], inline=False)
-            new_embed.add_field(name="💵 Payment", value=old_embed[0]['fields'][8]['value'], inline=False)
-            new_embed.add_field(name="🏢 Channel", value=f"<#{channel_id}>", inline=False)
-            new_embed.set_footer(text=old_embed[0]['footer']['text'])
+            new_embed.add_field(
+                name="👤 User", value=old_embed[0]["fields"][0]["value"], inline=False
+            )
+            new_embed.add_field(
+                name="🆔 User ID",
+                value=old_embed[0]["fields"][1]["value"],
+                inline=False,
+            )
+            new_embed.add_field(
+                name="📛 User name",
+                value=old_embed[0]["fields"][2]["value"],
+                inline=False,
+            )
+            new_embed.add_field(
+                name="👥 Claim user", value=interaction.user.mention, inline=False
+            )
+            new_embed.add_field(
+                name=f"{discord_emoji} Discord name",
+                value=old_embed[0]["fields"][4]["value"],
+                inline=False,
+            )
+            new_embed.add_field(
+                name=f"{roblox_emoji} Roblox username",
+                value=old_embed[0]["fields"][5]["value"],
+                inline=False,
+            )
+            new_embed.add_field(
+                name="🔢 Island Code",
+                value=old_embed[0]["fields"][6]["value"],
+                inline=False,
+            )
+            new_embed.add_field(
+                name="🏠 Build", value=old_embed[0]["fields"][7]["value"], inline=False
+            )
+            new_embed.add_field(
+                name="💵 Payment",
+                value=old_embed[0]["fields"][8]["value"],
+                inline=False,
+            )
+            new_embed.add_field(
+                name="🏢 Channel", value=f"<#{channel_id}>", inline=False
+            )
+            new_embed.set_footer(text=old_embed[0]["footer"]["text"])
             await queue_message.edit(embed=new_embed)
     except Exception as e:
         error_code_embed = discord.Embed(
@@ -141,8 +194,12 @@ async def claim_ticket(interaction: discord.Interaction, ticket_id = None):
             colour=discord.Colour(int("ff0000", 16)),
         )
         await interaction.followup.send(embeds=[error_code_embed], ephemeral=True)
-        print(f"Error when fetching queue message id for ticket {ticket_id['ticket_id']}: {e}")
+        print(
+            f"Error when fetching queue message id for ticket {ticket_id['ticket_id']}: {e}"
+        )
         return
 
     # Save to database
-    edit_builder_request_db(ticket_id=ticket_id["ticket_id"], claim_user_id=interaction.user.id)
+    edit_builder_request_db(
+        ticket_id=ticket_id["ticket_id"], claim_user_id=interaction.user.id
+    )
